8bb945333cd0a18c6c81ef9a9e7abdd28fd473e2
lucas <lcalandino@gmail.com>
2021-09-25 14:42:08 -0300

    change settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,21 +11,34 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+KEYSDIR = str(BASE_DIR)+"/keys.json"
+ 
+with open(KEYSDIR) as k:
+    project_keys = json.loads(k.read())
+ 
+def getKey(setting,project_keys=project_keys):
+    try:
+        return project_keys[setting]
+    except KeyError:
+        errorMessage = "Set the {} env var".format(setting)
+        raise ImproperlyConfigured(errorMessage)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eg)%wim==*%m^%p)f15y5m08ntb1j!_9fvt#yd)&&tdu*gkjsm'
+SECRET_KEY = getKey("SECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lucaskoblitz.pythonanywhere.com']
 
 
 # Application definition
